4bebbcfea2eac1d99c5670ca5b2399b13fb90da6
Wanes01 <emir.wanes.aouioua@gmail.com>
2024-11-06 17:53:06 +0100

    71: Iterable with policy completed

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -8,11 +8,20 @@ import it.unibo.inner.api.Predicate;
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
 
     private final T[] elements;
-    // private Predicate<T> predicate;
+    private Predicate<T> predicate;
 
     public IterableWithPolicyImpl(final T[] elements) {
+        this(elements, new Predicate<T>() {
+            @Override
+            public boolean test(T elem) {
+                return true;
+            }
+        });
+    }
+
+    public IterableWithPolicyImpl(final T[] elements, final Predicate<T> predicate) {
         this.elements = elements;
-        // this.predicate = null;
+        this.predicate = predicate;
     }
 
     @Override
@@ -22,22 +31,7 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
 
     @Override
     public void setIterationPolicy(Predicate<T> filter) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
-    }
-
-    private class ElementsIterator implements Iterator<T> {
-        private int current = 0;
-
-        @Override
-        public boolean hasNext() {
-            return this.current < elements.length;
-        }
-
-        @Override
-        public T next() {
-            return elements[this.current++];
-        }
+        this.predicate = filter;
     }
 
     @Override
@@ -49,6 +43,26 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
         }
         str += "]";
         return str;
+    }
+
+    private class ElementsIterator implements Iterator<T> {
+        private int current = 0;
+
+        @Override
+        public boolean hasNext() {
+            for (int i = this.current; i < elements.length; i++) {
+                if (predicate.test(elements[i])) {
+                    this.current = i;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public T next() {
+            return elements[this.current++];
+        }
     }
 
 }
